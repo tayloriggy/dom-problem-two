6560b463dfdb8319714c0ee1dd16c0c0b9a5b00d
Iggy36 <42382841+tayloriggy@users.noreply.github.com>
2021-08-29 21:01:29 -0500

    update

--- dom-three/script.js
+++ dom-three/script.js
@@ -1,10 +1,18 @@
-function playerLegends() {
-    var table = document.getElementById("baseball");
-    var row = table.insertRow(0);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(0);
-    var cell3 = row.insertCell(0);
-    cell1.innerHTML = "Ty Cobb";
-    cell2.innerHTML = "Detroit Tigers";
-    cell3.innerHTML = "Small ball intellectual";
+function addRow(baseball) {
+    let tableRef = document.getElementById("baseball");
+    
+    let newRow = tableRef.insertRow(4);
+
+    let cell1 = newRow.insertCell(0);
+    let cell2 = newRow.insertCell(1);
+    let cell3 = newRow.insertCell(2);
+    
+    let newText = document.createTextNode("Ty Cobb");
+    cell1.appendChild(newText);
+   
+    let newText = document.createTextNode("Detroit Tigers");
+    cell2.appendChild(newText);
+
+    let newText = document.createTextNode("Offensive Intellectual");
+    cell3.appendChild(newText);
 }
