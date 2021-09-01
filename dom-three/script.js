c4f4bbd42e1e0d6d4b41fc698d83a98f8564c075
Iggy36 <42382841+tayloriggy@users.noreply.github.com>
2021-08-31 22:01:39 -0500

    update

--- dom-three/script.js
+++ dom-three/script.js
@@ -1,18 +1,13 @@
-function addRow(baseball) {
-    let tableRef = document.getElementById("baseball");
+function addRow() {
+    let table = document.getElementById("baseball");
     
-    let newRow = tableRef.insertRow(4);
+    let row = table.insertRow(4);
 
-    let cell1 = newRow.insertCell(0);
-    let cell2 = newRow.insertCell(1);
-    let cell3 = newRow.insertCell(2);
+    let cell1 = row.insertCell(0);
+    let cell2 = row.insertCell(1);
+    let cell3 = row.insertCell(2);
     
-    let newText = document.createTextNode("Ty Cobb");
-    cell1.appendChild(newText);
-   
-    let newText = document.createTextNode("Detroit Tigers");
-    cell2.appendChild(newText);
-
-    let newText = document.createTextNode("Offensive Intellectual");
-    cell3.appendChild(newText);
+    cell1.innerHTML = "Ty Cobb";
+    cell2.innerHTML = "Detroit Tigers";
+    cell3.innerHTML = "Offensive Prowess";
 }
