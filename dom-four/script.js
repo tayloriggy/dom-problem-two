f575e411d1b3b620c16dccc64be12c4ae7952b76
Iggy36 <42382841+tayloriggy@users.noreply.github.com>
2021-09-01 22:45:43 -0500

    update

---
+++ dom-four/script.js
@@ -0,0 +1,6 @@
+function getHero () {
+    let x = document.getElementById("superhero").value;
+    let y = document.getElementById("superhero").length;
+    document.write("Number of items in dropdown link: " + y + " " + "Selected Hero: " + x );
+
+}
