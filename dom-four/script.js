cb852de8f2932418f495de79e19221f8753338ce
Iggy36 <42382841+tayloriggy@users.noreply.github.com>
2021-09-05 22:36:03 -0500

    update

--- dom-four/script.js
+++ dom-four/script.js
@@ -1,6 +1,11 @@
-function getHero () {
-    let x = document.getElementById("superhero").value;
-    let y = document.getElementById("superhero").length;
-    document.write("Number of items in dropdown link: " + y + " " + "Selected Hero: " + x );
+function getHero() {
+    alert("The items in this list are Thor, Iron Man, and Captain America. The number of items: 3");
+    let superheroes = ["Thor", "Iron Man", "Captain America"];
+    let newSuperheroes = superheroes.map(displayHeroes);
+    document.getElementById("superhero").innerHTML = newSuperheroes.join(" ");
+}
 
+function displayHeroes(hero) {
+    let hero = document.getElementById("superhero");
+    document.getElementById("superhero").innerHTML = hero;
 }
