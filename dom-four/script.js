68ac1de29d28b586294130a6ff5a2bb291498446
Iggy36 <42382841+tayloriggy@users.noreply.github.com>
2021-09-07 22:07:04 -0500

    update

--- dom-four/script.js
+++ dom-four/script.js
@@ -1,11 +1,15 @@
+var heroes = ["Thor", "Iron Man", "Captain America"];
+var newHeroes = heroes.map(getHero);
+newHeroes.join("Thor, Iron Man, Captain America");
+
+
 function getHero() {
-    alert("The items in this list are Thor, Iron Man, and Captain America. The number of items: 3");
-    let superheroes = ["Thor", "Iron Man", "Captain America"];
-    let newSuperheroes = superheroes.map(displayHeroes);
-    document.getElementById("superhero").innerHTML = newSuperheroes.join(" ");
+    let select = document.getElementById("superhero");
+    let text = select.options[select.selectedIndex].text;
+    console.log(text);
+    alert("Thor, Iron Man, Captain America");
 }
 
-function displayHeroes(hero) {
-    let hero = document.getElementById("superhero");
-    document.getElementById("superhero").innerHTML = hero;
-}
+getHero();
+
+
