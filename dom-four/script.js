35ab9d8c8da8250ffe97be3db4500ee6bbaac389
Iggy36 <42382841+tayloriggy@users.noreply.github.com>
2021-09-12 14:26:13 -0500

    update

--- dom-four/script.js
+++ dom-four/script.js
@@ -1,12 +1,12 @@
 function getHero() {
-    let select = document.getElementById('superhero');
+    let select = document.getElementById('heroes');
     let optionValues = [];
     for (let i = 0; i <select.options.length; i++) {
         optionValues.push(select.options[i].value);
     }
 
     alert(optionValues.join(", "));
-    alert("There are three options to choose from.");
+    alert(`There are ${optionValues.length} options to choose from.`);
 }
 
 
